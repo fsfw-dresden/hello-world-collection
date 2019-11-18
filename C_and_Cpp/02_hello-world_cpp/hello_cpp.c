c9f41efbea2fef5a7cb68b89e4328827e7cdfd84
Carsten Knoll <carsten.knoll@posteo.de>
2019-11-18 23:58:46 +0100

    add cpp example

---
+++ C_and_Cpp/02_hello-world_cpp/hello_cpp.c
@@ -0,0 +1,17 @@
+// source: https://en.wikibooks.org/wiki/C%2B%2B_Programming/Examples/Hello_world
+
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello World from C++!" << std::endl;
+  return 0;
+}
+
+
+// build command:
+// g++ hello_cpp.c -o hello_cpp
+
+// Run the program:
+// ./hello_cpp
